303ad8f383f2c510e55970e42c7a23c15b66b39f
Fabian Lupa <f.lupa@flaiker.com>
2015-07-23 18:01:38 +0200

    Database access using doctrine orm

---
+++ config/cli-config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Helper file for database schema update
+ * Use "vendor\bin\doctrine orm:schema-tool:update --force --dump-sql" to update db-schema
+ * or "vendor\bin\doctrine orm:schema-tool:create" to create a new schema
+ */
+
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use KCMS\Config;
+
+require __DIR__ . "/../bootstrap.php";
+
+return ConsoleRunner::createHelperSet(\KCMS\Database\DbService::getEntityManager());
