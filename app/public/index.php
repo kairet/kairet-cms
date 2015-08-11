822504d5fec141d6932417d714595051848e783b
Fabian Lupa <f.lupa@flaiker.com>
2015-08-11 21:11:49 +0200

    Routing using silex

--- app/public/index.php
+++ app/public/index.php
@@ -1,11 +1,33 @@
 <?php
-use KCMS\Api\ApiHelper;
-
 require __DIR__ . "/../../bootstrap.php";
 
-$args = array_merge($_POST, $_GET);
+$app = new Silex\Application();
 
-$apiHelper = new ApiHelper();
-$apiHelper->handleRequest($args);
+// Setup silex configuration
+$app['debug'] = \KCMS\Config::DEV_MODE;
+$app->register(new Silex\Provider\ServiceControllerServiceProvider());
 
-echo $apiHelper->getJsonReponse();
+// Add services
+$app['entityManager'] = $app->share(function () {
+    return \KCMS\Database\DbService::getEntityManager();
+});
+
+// Setup controllers
+
+// Add user controller as a service
+$app['users.controller'] = $app->share(function () use ($app) {
+    return new \KCMS\Controller\UserController($app['entityManager']);
+});
+
+// Define routes for user controller
+/** @var \Silex\ControllerCollection $user */
+$user = $app['controllers_factory'];
+$user->get('/', 'users.controller:getAllUsersJson');
+$user->get('/{id}', 'users.controller:getUserJson');
+
+// Mount user controller
+$app->mount('/users', $user);
+
+
+// Start the application
+$app->run();
