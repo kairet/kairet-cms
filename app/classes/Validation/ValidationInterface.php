b86e9647a0d4907e1cb5be354ad630333e231972
Fabian Lupa <f.lupa@flaiker.com>
2015-09-03 21:01:52 +0200

    Extract model validation implementation to base class

--- app/classes/Validation/ValidationInterface.php
+++ app/classes/Validation/ValidationInterface.php
@@ -1,23 +1,15 @@
 <?php
 namespace KCMS\Validation;
 
-use Doctrine\ORM\Mapping as ORM;
-
 /**
- * Indicates an object implements validation logic, also sets doctrine lifecycle-annotations (logic must be checked
- * against on database insert or update)
+ * Indicates an object implements validation logic
  *
- * @ORM\MappedSuperclass
- * @ORM\HasLifecycleCallbacks
  * @package KCMS\Validation
  */
 interface ValidationInterface
 {
     /**
      * Validate an object, throw {@see ValidationException} if invalid
-     *
-     * @ORM\PrePersist
-     * @ORM\PreUpdate
      *
      * @throws ValidationException
      */
